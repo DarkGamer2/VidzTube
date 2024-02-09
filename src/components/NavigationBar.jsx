85c654156202f6432f87235476ecef632fbdeeb5
Kameer Hosein <kameer-hosien@outlook.com>
2024-02-09 12:13:24 -0400

    first commit

---
+++ src/components/NavigationBar.jsx
@@ -0,0 +1,14 @@
+const NavigationBar = () => {
+  return (
+    <div>
+      <nav>
+        <ul className="flex">
+          <li>Home</li>
+          <li>Videos</li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default NavigationBar;
